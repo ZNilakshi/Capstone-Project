3d46c302eda722639e8390cf024a1f3cc8df528d
Hiruni Wijerathna <114440722+HiruniWijerathna@users.noreply.github.com>
2025-03-07 00:01:52 +0530

    product

--- frontend/src/components/FeaturedProducts.jsx
+++ frontend/src/components/FeaturedProducts.jsx
@@ -6,37 +6,57 @@ const FeaturedProducts = () => {
 
     return (
         <div className="bg-black text-white px-4 py-6">
-            <h2 className="text-center text-xl font-semibold mb-4">
-                FEATURED PRODUCTS
-            </h2>
-            <div className="flex flex-wrap justify-center gap-2 md:gap-4 mb-4">
+
+        <section className="flex flex-col md:flex-row items-center justify-center    bg-black">
+            <div className="w-full  pr-6 mr-6 md:w-1/2 md:pl-12 mt-6 md:mt-0 text-center ">
+             <h2 className="text-3xl md:text-4xl font-bold text-white mb-4">FEATURED PRODUCTS</h2>
+            </div>
+        </section>
+           
+            
+            
+            <div className="flex flex-wrap justify-center gap-8 md:gap-16 mb-4">
                 <button
-                    className="bg-orange-500 text-black px-8 md:px-16 py-3 md:py-4 font-semibold border border-black hover:bg-white hover:text-orange-500 transition"
+                    className="bg-orange-500 text-black px-8 md:px-5 py-3 md:py-4 font-semibold border border-black hover:bg-white hover:text-orange-500 transition"
                     onClick={() => navigate("/spirit")}
                 >
                     SPIRIT
+                    <div className="flex justify-center">
+                         <img
+                         src="/sprit.jpg"
+                         alt="sprit"
+                         className="w-40 max-w-2xl h-60 object-cover"
+                         />
+                    </div>
                 </button>
                 <button
-                    className="bg-orange-500 text-black px-4 md:px-6 py-3 md:py-4 font-semibold border border-black hover:bg-white hover:text-orange-500 transition"
+                    className=" bg-orange-500 text-black px-4 md:px-5 py-3 md:py-4 font-semibold border border-black hover:bg-white hover:text-orange-500 transition"
                     onClick={() => navigate("/shake-and-beer")}
                 >
                     SHAKE AND BEER
+                    <div className="flex justify-center">
+                         <img
+                         src="/beer.jpg"
+                         alt="beer"
+                         className="w-40 max-w-2xl h-60 object-cover"
+                         />
+                    </div>
                 </button>
                 <button
-                    className="bg-orange-500 text-black px-8 md:px-16 py-3 md:py-4 font-semibold border border-black hover:bg-white hover:text-orange-500 transition"
+                    className="bg-orange-500 text-black px-8 md:px-5 py-3 md:py-4 font-semibold border border-black hover:bg-white hover:text-orange-500 transition"
                     onClick={() => navigate("/WineStore")}
                 >
                     WINE
+                    <div className="flex justify-center">
+                         <img
+                         src="/wine.jpg"
+                         alt="wine"
+                         className="w-40 max-w-2xl h-60 object-cover"
+                         />
+                    </div>
                 </button>
             </div>
-            <div className="flex justify-center">
-                <img
-                    src="/back.jpg"
-                    alt="Featured Products"
-                    className="w-full max-w-3xl h-auto object-cover"
-                />
-            </div>
-        </div>
+         </div>
     );
 };
 
--- frontend/src/components/ShooByBrand.jsx
+++ frontend/src/components/ShooByBrand.jsx
@@ -1,16 +1,63 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const ShooByBrand  = () => {
-  return (
-    <section className="flex flex-col md:flex-row items-center justify-center    bg-black">
-      
-        <div className="w-full  pr-6 mr-6 md:w-1/2 md:pl-12 mt-6 md:mt-0 text-center ">
-        <h2 className="text-3xl md:text-4xl font-bold text-white mb-4">SHOP BY BRAND</h2>
-        
-      </div>
-     
-    </section>
-  );
+const ShooByBrand = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="bg-black text-white px-4 py-6">
+
+        <section className="flex flex-col md:flex-row items-center justify-center    bg-black">
+            <div className="w-full  pr-6 mr-6 md:w-1/2 md:pl-12 mt-6 md:mt-0 text-center ">
+             <h2 className="text-3xl md:text-4xl font-bold text-white mb-4">SHOP BY BRAND</h2>
+            </div>
+        </section>
+           
+            
+            
+            <div className="flex flex-wrap justify-center gap-8 md:gap-16 mb-4">
+                <button
+                    className="bg-orange-500 text-black px-8 md:px-5 py-3 md:py-4 font-semibold border border-black hover:bg-white hover:text-orange-500 transition"
+                    onClick={() => navigate("/spirit")}
+                >
+                    SPIRIT
+                    <div className="flex justify-center">
+                         <img
+                         src="/sprit.jpg"
+                         alt="sprit"
+                         className="w-40 max-w-2xl h-60 object-cover"
+                         />
+                    </div>
+                </button>
+                <button
+                    className=" bg-orange-500 text-black px-4 md:px-5 py-3 md:py-4 font-semibold border border-black hover:bg-white hover:text-orange-500 transition"
+                    onClick={() => navigate("/shake-and-beer")}
+                >
+                    SHAKE AND BEER
+                    <div className="flex justify-center">
+                         <img
+                         src="/beer.jpg"
+                         alt="beer"
+                         className="w-40 max-w-2xl h-60 object-cover"
+                         />
+                    </div>
+                </button>
+                <button
+                    className="bg-orange-500 text-black px-8 md:px-5 py-3 md:py-4 font-semibold border border-black hover:bg-white hover:text-orange-500 transition"
+                    onClick={() => navigate("/WineStore")}
+                >
+                    WINE
+                    <div className="flex justify-center">
+                         <img
+                         src="/wine.jpg"
+                         alt="wine"
+                         className="w-40 max-w-2xl h-60 object-cover"
+                         />
+                    </div>
+                </button>
+            </div>
+         </div>
+    );
 };
 
 export default ShooByBrand ;
