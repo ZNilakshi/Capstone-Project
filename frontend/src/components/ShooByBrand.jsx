3ceb908839957ab919f3df8cf2f333d055cebb85
Hiruni Wijerathna <114440722+HiruniWijerathna@users.noreply.github.com>
2025-03-06 19:55:48 +0530

    Update ShooByBrand.jsx

--- frontend/src/components/ShooByBrand.jsx
+++ frontend/src/components/ShooByBrand.jsx
@@ -5,7 +5,7 @@ const ShooByBrand  = () => {
     <section className="flex flex-col md:flex-row items-center justify-center    bg-black">
       
         <div className="w-full  pr-6 mr-6 md:w-1/2 md:pl-12 mt-6 md:mt-0 text-center ">
-        <h2 className="text-3xl md:text-4xl font-bold text-white mb-4">ABOUT US</h2>
+        <h2 className="text-3xl md:text-4xl font-bold text-white mb-4">SHOP BY BRAND</h2>
         
       </div>
      
