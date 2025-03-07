dba12d7325c617e81819c33f01fe76b1084f653b
Wathsu4 <wathsalathathsarani15@gmail.com>
2025-03-07 19:33:06 +0530

    Account page frontend part

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,20 @@
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AgeVerification from "./components/AgeVerification";
 import WineStore from "./pages/WineStore";
-import './App.css';
-
+import "./App.css";
+import Account from "./components/Account";
 function App() {
-  
   return (
     <Router>
       <Navbar />
       <AgeVerification />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/WineStore" element={<WineStore/>} />
-      
+        <Route path="/WineStore" element={<WineStore />} />
+        <Route path="/Account" element={<Account />} />
       </Routes>
       <Footer />
     </Router>
